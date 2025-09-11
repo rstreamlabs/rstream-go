eeb5271a7736a6b7581b523a2f24c092d4d609b0
uartnet <140632163+uartnet@users.noreply.github.com>
2025-09-11 11:47:53 +0200

    feat: add login, logout cmds

--- cmd/rstream/cmd/logout.go
+++ cmd/rstream/cmd/logout.go
@@ -3,14 +3,30 @@
 package cmd
 
 import (
+	"context"
+	"fmt"
+	"os"
+
 	"github.com/spf13/cobra"
 )
 
 var logoutCmd = &cobra.Command{
-	GroupID: "common",
-	Use:     "logout",
-	Short:   "Log out from the rstream engine",
-	Run:     func(cmd *cobra.Command, args []string) {},
+	GroupID:      "common",
+	Use:          "logout",
+	Short:        "Log out from the rstream engine",
+	SilenceUsage: true,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		client, err := newClientFromFlags(cmd)
+		if err != nil {
+			return err
+		}
+		host, err := client.Logout(context.Background())
+		if err != nil {
+			return fmt.Errorf("logout failed: %w", err)
+		}
+		fmt.Fprintf(os.Stdout, "Logged out from host %s\n", *host)
+		return nil
+	},
 }
 
 func init() {
