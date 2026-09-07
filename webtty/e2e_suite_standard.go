51beac2241d7655e7c5f36bb0a1e3a637f8b6b41
uartnet <140632163+uartnet@users.noreply.github.com>
2026-09-07 18:30:49 +0200

    fix(fips): satisfy strict profile static analysis

--- webtty/e2e_suite_standard.go
+++ webtty/e2e_suite_standard.go
@@ -6,9 +6,20 @@ package webtty
 
 import (
 	"crypto/tls"
+	"fmt"
 
 	"github.com/rstreamlabs/rstream-go/internal/fipsprofile"
 )
+
+func validateSupportedKeyEnvelopeSuite(suite KeyEnvelopeSuite) error {
+	switch suite {
+	case KeyEnvelopeSuiteHPKEX25519HKDFSHA256AES256GCM,
+		KeyEnvelopeSuiteP256HKDFSHA256AES256GCMRandomNonce:
+		return nil
+	default:
+		return fmt.Errorf("unsupported E2E key envelope suite %d", suite)
+	}
+}
 
 func defaultE2EKeyEnvelopeSuite() KeyEnvelopeSuite {
 	return KeyEnvelopeSuiteHPKEX25519HKDFSHA256AES256GCM
